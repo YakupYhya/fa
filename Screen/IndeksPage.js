9a70d55ba48f8886abd4cf22a3b57cbf066c9493
YakupYhya <yakupyhya1@cmail.com>
2024-04-25 21:18:48 +0300

    IndeksPage oluşturuldu

---
+++ Screen/IndeksPage.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const IndeksPage = () => {
+  return (
+    <View>
+      <Text>IndeksPage</Text>
+    </View>
+  )
+}
+
+export default IndeksPage
