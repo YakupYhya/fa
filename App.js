234e1e281c17d8f59f25c85b6b1abe064d6407d7
YakupYhya <yakupyhya1@cmail.com>
2024-05-02 23:40:00 +0300

    bağlantı düzenlemeleri yapıldı

--- App.js
+++ App.js
@@ -8,6 +8,20 @@ import BesinlerPage from './Screen/BesinlerPage';
 import AntrenmanPage from './Screen/AntrenmanPage';
 import ProfilPage from './Screen/ProfilPage';
 import IndeksPage from './Screen/IndeksPage';
+import { firebase } from '@react-native-firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsA9EBrSbCRg-n_PFz08J_JHgziZtSiH4",
+  authDomain: "fitnessapp-f2fa2.firebaseapp.com",
+  projectId: "fitnessapp-f2fa2",
+  storageBucket: "fitnessapp-f2fa2.appspot.com",
+  messagingSenderId: "32045173210",
+  appId: "1:32045173210:web:62470820297cc2f90518e3",
+  measurementId: "G-29XDN3L1VM"
+};
+
+
+
 export default function App() {
   const Stack = createNativeStackNavigator();
 
---
+++ Screen/DataConnection.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDsA9EBrSbCRg-n_PFz08J_JHgziZtSiH4",
+    authDomain: "fitnessapp-f2fa2.firebaseapp.com",
+    projectId: "fitnessapp-f2fa2",
+    storageBucket: "fitnessapp-f2fa2.appspot.com",
+    messagingSenderId: "32045173210",
+    appId: "1:32045173210:web:62470820297cc2f90518e3",
+    measurementId: "G-29XDN3L1VM"
+  };  
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
