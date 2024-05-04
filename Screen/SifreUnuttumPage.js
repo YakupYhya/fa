6a57a786a066bfca2e1ed84e9c655c144749b7a2
YakupYhya <yakupyhya1@cmail.com>
2024-05-04 21:56:58 +0300

    SifreUnuttumPage Tasarlandı

--- Screen/SifreUnuttumPage.js
+++ Screen/SifreUnuttumPage.js
@@ -1,12 +1,70 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 
-const SifreUnuttumPage = () => {
+import React, { useState } from 'react';
+ 
+export default function ForgotPasswordPage({navigation}) {
+
+    const [email, setEmail] = useState("");
+
   return (
-    <View>
-      <Text>SifreUnuttumPage</Text>
+    <View style={styles.container}>
+
+        <TextInput
+          placeholder='E-posta Adresiniz'  
+          style={styles.textInputStyle}
+          onChangeText={setEmail}
+          value={email}
+        />
+       
+        <TouchableOpacity onPress={() => { /* Şifre Gönderme İşlemi olucak */ }} style={styles.button}>
+          <Text style={styles.buttonText}>Şifre Gönder</Text>
+        </TouchableOpacity>
+
+        <StatusBar style="auto" />
     </View>
   )
 }
 
-export default SifreUnuttumPage
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor:"#f7fff7",
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    header: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 30,
+    },
+    textInputStyle:{
+      width:'70%',
+      height:50,
+      borderRadius: 40,
+      marginVertical: 10,
+      paddingHorizontal: 20,
+      backgroundColor:"#d8f3dc",
+    },
+    button:{
+      width:"70%",
+      height: 40,
+      alignItems:'center',
+      justifyContent:"center",
+      borderRadius: 50,
+      marginVertical: 20,
+      backgroundColor:"#C1FF72",
+      opacity: 0.9,
+    },
+    buttonText:{
+      fontWeight:"bold",
+      fontSize: 18,
+      color:"#018B49",
+    },
+});
