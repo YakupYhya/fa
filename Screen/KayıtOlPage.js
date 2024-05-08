0f07bc29a156af545bf6644e67ea12d26265691c
YakupYhya <yakupyhya1@cmail.com>
2024-05-08 17:46:39 +0300

    KayıtOlPage arayüz tasarımı yapıldı

--- Screen/KayıtOlPage.js
+++ Screen/KayıtOlPage.js
@@ -1,12 +1,100 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// KayıtOlPage.js
 
-const KayıtOlPage = () => {
+import { StatusBar } from 'expo-status-bar';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity
+ } from 'react-native';
+
+import React, { useState } from 'react';
+
+const KayıtOlPage = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text>KayıtOl</Text>
+    <View style={styles.container}>
+      <Image 
+        style={styles.logo}
+        source={require('./resimler/lol.png')}
+      /> 
+
+      <TextInput
+        placeholder='Kullanıcı Adı'  
+        style={styles.textInputStyle}
+        onChangeText={setName}
+        value={name}
+      />
+
+      <TextInput
+        placeholder='Email'  
+        style={styles.textInputStyle}
+        onChangeText={setEmail}
+        value={email}
+        keyboardType='email-address'
+      />
+    
+      <TextInput 
+        placeholder='Şifre'
+        style={styles.textInputStyle}
+        onChangeText={setPassword}
+        value={password} 
+        secureTextEntry={true}   
+      />
+
+      <TouchableOpacity
+        onPress={()=> navigation.navigate('Login')}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}> Kayıt Ol </Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
     </View>
   )
 }
 
-export default KayıtOlPage
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:"#f7fff7",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 300,
+    height: 250,
+    marginBottom: 70,
+  },
+  textInputStyle:{
+    width:'70%',
+    height:50,
+    borderRadius: 40,
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor:"#d8f3dc",
+  },
+  button:{
+    width:"40%",
+    height: 40,
+    alignItems:'center',
+    justifyContent:"center",
+    borderRadius: 50,
+    marginVertical: 30,
+    backgroundColor:"#C1FF72",
+    opacity: 0.9,
+  },
+  buttonText:{
+    fontWeight:"bold",
+    fontSize: 25,
+    color:"#018B49",
+  },
+
+});
+
+export default KayıtOlPage;
